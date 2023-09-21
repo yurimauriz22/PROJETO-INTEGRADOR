1ca92e22404e5802999e22a2eaadd19e85b0a3b0
Yuri <franciscoyuridasilvamauriz@gmail.com>
2023-09-21 19:49:54 -0300

    primeiro

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+document.getElementById("enviar").addEventListener("click", function () {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    if (email !== "" && senha !== "") {
+        if (email.includes("@") && email.includes(".")) {
+            // Email válido, redirecionar para a página de início
+            window.location.href = "inicio.html";
+        } else {
+            alert("Por favor, insira um email válido.");
+        }
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
+});
